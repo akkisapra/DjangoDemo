ee6eca1b367a2126568189bc3f69230d072454c1
Akshay Sapra <akshay.sapra@rocketmail.com>
2020-06-15 04:05:28 +1000

    adding test suite

--- octopusTest/octTest/tests.py
+++ octopusTest/octTest/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from selenium import webdriver
+
+class FunctionalTestCase(TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_there_is_homepage(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn("hello", self.browser.page_source)
+
+class UnitTestCase(TestCase):
+    def test_home_homepage_template(self):
+        response =self.client.get("/")
+        self.assertTemplateUsed(response,'test/home.html')
+
+
