80edf514613650fb4bdeb8d39a02ba3376d93793
Akshay Sapra <akshay.sapra@rocketmail.com>
2020-06-15 02:35:54 +1000

    adding search fields

--- octopusTest/octTest/admin.py
+++ octopusTest/octTest/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import bill
+
+@admin.register(bill)
+class billAdmin(admin.ModelAdmin):
+    list_display = ["NMI","serial","readingValue","date","fileName"]
+    search_fields = ("NMI","serial")
